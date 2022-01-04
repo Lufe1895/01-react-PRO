86e2bb94d23e4bffbdec516e5f1132e39e7e810a
Luis Fernando Antonio Toral <luislecompt@gmail.com>
2022-01-04 17:51:38 -0600

    primer commit

---
+++ src/bases/Counter.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+interface Props {
+    initialValue:number
+}
+
+export const Counter = ({ initialValue }:Props) => {
+    const [counter, setCounter] = useState<number>(initialValue);
+
+    const handleIncrement = () => {
+        setCounter(c => c + 1);
+    }
+
+    return (
+        <>
+            <h1>Counter: { counter }</h1>
+
+            <button onClick={ handleIncrement }>
+                +1
+            </button>
+        </>
+    )
+}
